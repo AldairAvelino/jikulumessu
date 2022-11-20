dcdb4a65fb2caa81e7966563815843eb7720b778
Aldair Avelino <aldair03avelino@gmail.com>
2022-11-20 23:41:43 +0100

    Menu improvisado criado - AA

--- src/pages/auth/sign-up-admin.php
+++ src/pages/auth/sign-up-admin.php
@@ -11,6 +11,25 @@
     <title>Sign Up | ADMIN</title>
 </head>
 <body class="text-center">
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="ml-auto collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="sign-up-admin.php">Administrator</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="sign-up-user.php">Normal User</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
     <form class="form-signin" action="../../actions/auth/sign-up-admin.php" method="POST">
         <img class="logo" src="../../../assets/images/logo/logo.png" alt="logo">
         <h1 class="h3 mb-3 font-weight-normal">Cadastrar Admin</h1>
