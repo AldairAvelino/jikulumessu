4b6fc9c9b15b1164c0d7142fce6c6c7ab4a144be
Aldair Avelino <aldair03avelino@gmail.com>
2022-11-20 15:49:57 +0100

    Arquivo de conexão ao banco de dados - AA

---
+++ src/actions/connection.php
@@ -0,0 +1,11 @@
+<?php
+    $HOST = "localhost";
+    $ROOT = "root";
+    $PASSWORD = "";
+    $DATABASE = "jikulumessu";
+    
+    $connection = mysqli_connect($HOST, $ROOT, $PASSWORD, $DATABASE);
+
+    if(mysqli_connect_error()){
+        echo "FALHA AO CONECTAR-SE A BASE DE DADOS";
+    }
