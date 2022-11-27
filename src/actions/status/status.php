83ad90fd4248e8e5f3e6bc7a8738cc3082b1cbb7
Aldair Avelino <aldair03avelino@gmail.com>
2022-11-27 22:18:14 +0100

    Adicionado os retornos para mensagens de erro e/ou sucesso - AA

--- src/actions/status/status.php
+++ src/actions/status/status.php
@@ -2,6 +2,7 @@
     include("../../actions/connection.php");
 
     echo("<link rel='stylesheet' href='../../../assets/bootstrap-4.6.2-dist/css/bootstrap.css'>");
+    echo("<link rel='stylesheet' href='../../../assets/css/success-consulta.css'>");
     echo("<script src='../../../assets/bootstrap-4.6.2-dist/js/bootstrap.js'></script>");
 
     $consultar = mysqli_real_escape_string($connection, $_POST['consultar']);
@@ -14,18 +15,34 @@
         $result = mysqli_fetch_array($search);
 
         if ($search && mysqli_num_rows($search) == 1) {     
-            echo("<p>Bilhete de Identidade número: {$bi}</p>");
-
-            echo("<p>Nome do usuário: {$result['name']}</p>");
-            
-            echo("<p>Status do usuário: {$result['status']}</p>");
-
-            echo "<a href = \"../../pages/auth/status-consult-user.php\" classe=\"text-center\">Consular outro número</a>";
+            echo("
+                <div class='text-center'>
+                    <div class='modal-dialog modal-confirm'>
+                        <div class='modal-content'>
+                            <div class='modal-header'>
+                                <div class='icon-box'>
+                                    <i class='material-icons'>&checkmark;</i>
+                                </div>				
+                                <h4 class='modal-title w-100'>Resultado da Consultar</h4>	
+                            </div>
+                
+                            <div class='modal-body'>
+                                <p class='text-center'>Nome: {$result['name']}</p>
+                                <p class='text-center'>Bilhete de Identidade número: {$bi}</p>
+                                <p class='text-center'>Status: {$result['status']}</p>
+                            </div>
+                            
+                            <div class='modal-footer'>
+                                <a class='btn btn-block' href='../../pages/auth/status-consult-user.php'>Consultar outro número</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>     
+            ");
 
         } else {
             // falhou o login
-            echo "<p>Esse dado que inseriste não se encontra na nossa base de dados</p>";
-            echo "<a href = \"../../pages/auth/status-consult-user.php\" classe=\"text-center\">Insira corretamente, e tente novamente</a>";
+            header("Location: ../../pages/error-reserve.php");
         }
         
     }
---
+++ src/pages/success-consulta.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="../../assets/css/success-consulta.css">
+<title>Bootstrap Simple Error Confirmation Popup</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+<body>
+<!-- Modal HTML -->
+<div class="text-center">
+    <div class="modal-dialog modal-confirm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="icon-box">
+                    <i class="material-icons">&checkmark;</i>
+                </div>				
+                <h4 class="modal-title w-100">Resultado da Consultar</h4>	
+            </div>
+
+            <div class="modal-body">
+                <p class="text-center">O dado inserido não foi encontrado base de dados, por favor tente novamente e insira dados corretos!</p>
+            </div>
+
+            <div class="modal-footer">
+                <a class="btn btn-danger btn-block" href="auth/status-consult-user.php">Consultar outro número</a>
+            </div>
+        </div>
+    </div>
+</div>     
+</body>
+</html>                 
